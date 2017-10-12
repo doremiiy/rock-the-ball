0acde38c54f6024c80cc795840ba3f995d2bc741
remid0 <remid.2002@gmail.com>
2017-10-12 10:12:15 +0200

    network manager & testBall

---
+++ Assets/Scripts/Test/TestBall.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBall : MonoBehaviour {
+
+	void Start () {
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.AddForce (new Vector3 (100, 0, 0));
+	}
+}
