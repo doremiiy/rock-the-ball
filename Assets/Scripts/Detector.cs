5c0c681ab98c357717eafb945b7db4f125eb5216
Louis Groisne <louis.groisne@gmail.com>
2017-10-12 19:40:24 +0200

    Add a deadzone with areas that trigger the apparition of its limits

---
+++ Assets/Scripts/Detector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detector : MonoBehaviour {
+
+    public GameObject limit;
+    private Renderer limitTexture;
+
+
+    private void Start()
+    {
+        limitTexture = limit.GetComponent<Renderer>();
+        limitTexture.material.SetFloat("_Cutoff", 1f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player is too close to the wall");
+            limitTexture.material.SetFloat("_Cutoff", 06716418f);
+        }
+    }
+}
