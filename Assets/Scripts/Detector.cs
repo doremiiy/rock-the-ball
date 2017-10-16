29f054274baec31a0335165f52f3232a8d49a2df
Louis Groisne <louis.groisne@gmail.com>
2017-10-16 09:39:50 +0200

    Add deadzone apparition and disparition

--- Assets/Scripts/Detector.cs
+++ Assets/Scripts/Detector.cs
@@ -6,20 +6,51 @@ public class Detector : MonoBehaviour {
 
     public GameObject limit;
     private Renderer limitTexture;
+    public float cutoffSup;
+    public float cutoffInf;
+    public float cutoffDefault;
+    private bool isAppearing = false;
+    private bool isDisappearing = false;
+    private float lerpValue = 0.0f;
+    public float speedApparitionMultiplier;
 
 
     private void Start()
     {
         limitTexture = limit.GetComponent<Renderer>();
-        limitTexture.material.SetFloat("_Cutoff", 1f);
+        limitTexture.material.SetFloat("_Cutoff", cutoffDefault);
+    }
+
+    private void Update()
+    {
+        if (isAppearing)
+        {
+            limitTexture.material.SetFloat("_Cutoff", Mathf.Lerp(cutoffSup, cutoffInf, lerpValue));
+            lerpValue += Time.deltaTime * speedApparitionMultiplier;
+        } else if (isDisappearing)
+        {
+            limitTexture.material.SetFloat("_Cutoff", Mathf.Lerp(cutoffInf, cutoffSup, lerpValue));
+            lerpValue += Time.deltaTime * speedApparitionMultiplier;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player is too close to the wall");
-            limitTexture.material.SetFloat("_Cutoff", 06716418f);
+            lerpValue = 0f;
+            isAppearing = true;
+            isDisappearing = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            lerpValue = 0f;
+            isDisappearing = true;
+            isAppearing = false;
         }
     }
 }
