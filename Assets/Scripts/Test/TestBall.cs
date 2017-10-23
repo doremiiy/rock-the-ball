6412454b80193626701eb33ed184a7237dddac69
Louis Groisne <louis.groisne@gmail.com>
2017-10-23 11:47:41 +0200

    Add a speed cap in the TestBall script

--- Assets/Scripts/Test/TestBall.cs
+++ Assets/Scripts/Test/TestBall.cs
@@ -6,12 +6,19 @@ public class TestBall : MonoBehaviour {
 
 	private Rigidbody rb;
 	public Vector3 force;
-	void Start () {
+    public float maxSpeed;
+
+    void Start () {
 		rb = GetComponent<Rigidbody>();
 		rb.AddForce (force);
 	}
 
-	void FixedUpdate () {
-		
-	}
+    void FixedUpdate()
+    {
+        //Debug.Log(rb.velocity);
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
 }
