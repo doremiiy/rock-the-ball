b9e81c1b2408d08e2019dfec9181ac107a6e0fd9
remid0 <remid.2002@gmail.com>
2017-10-23 11:56:23 +0200

    line renderer

--- Assets/Scripts/LaserPointer.cs
+++ Assets/Scripts/LaserPointer.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class LaserPointer : MonoBehaviour {
 
+	private LineRenderer laserLineRenderer;
+	public float laserWidth = 0.1f;
+	public float laserMaxLength = 5f;
+
+	void Start ()
+	{
+		laserLineRenderer = GetComponent<LineRenderer> ();
+		laserLineRenderer.SetWidth (laserWidth, laserWidth);
+	}		
+
 	void FixedUpdate()
 	{
-		Vector3 fwd = transform.TransformDirection(Vector3.forward);
-
-		if (Physics.Raycast(transform.position, fwd, 10))
-			print("There is something in front of the object!");
+		Ray ray = new Ray (transform.position, transform.forward);
+		//RaycastHit raycastHit;
+		laserLineRenderer.SetPosition( 0, transform.position );
+		laserLineRenderer.SetPosition( 1, transform.forward );
+		//RaycastHit hit;
+		//R
+		//Debug.DrawRay (transform.position, transform.forward);
+		//if (Physics.Raycast(ray, out hit))
+		//	Debug.Log("There is something in front of the object!");
 	}
 }
