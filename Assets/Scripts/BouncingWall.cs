7ffcd3c97135cc41fa3c6a6bd22418bcdf88106c
Louis Groisne <louis.groisne@gmail.com>
2017-10-23 11:37:05 +0200

    Add the bouncingWall Scripts to handle the ball more efficiently

---
+++ Assets/Scripts/BouncingWall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingWall : MonoBehaviour {
+
+    private Vector3 wallNormal = new Vector3();
+    public float BouncingMultiplier;
+
+
+    private void Start()
+    {
+        wallNormal = transform.up;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TEST");
+        if (other.CompareTag("Ball"))
+        {
+            Rigidbody ballRigidbody = other.GetComponent<Rigidbody>();
+            Vector3 ballSpeed = ballRigidbody.velocity;
+            ballSpeed = Vector3.Project(ballSpeed, wallNormal);
+            Vector3 force = -ballSpeed * BouncingMultiplier;
+            ballRigidbody.AddForce(force, ForceMode.Impulse);
+        }
+    }
+}
