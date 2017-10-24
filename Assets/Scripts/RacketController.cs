1266080239ac49d00aae3c4dadd0ec380f1da207
Rémi Di Vita <remid.2002@gmail.com>
2017-10-24 17:27:10 +0200

    refactor player controller

---
+++ Assets/Scripts/RacketController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketController : MonoBehaviour {
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = transform.parent.gameObject;
+    }
+
+	void OnTriggerEnter (Collider collider) {
+        player.GetComponent<PlayerController>().ballHit(this.gameObject, collider);
+	}
+}
