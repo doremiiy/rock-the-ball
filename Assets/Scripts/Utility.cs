e49241c360e4db2b71043a7ec0d01a5bbed57758
Louis Groisne <louis.groisne@gmail.com>
2017-10-24 10:08:40 +0200

    add an OppositePlayer in the Utility class

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -1,8 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Utility {
     public enum Team { blue, red };
     public enum HandController { right, left };
+
+    public Team Opp(Team team)
+    {
+        Team opp;
+        if  (team == Team.blue)
+        {
+            opp = Team.red;
+        } else if (team == Team.red)
+        {
+            opp = Team.blue;
+        } else
+        {
+            throw new Exception("Error : Unrecognized Team passed to method Utility.Opp()");
+        }
+        return opp;
+    }
 }
