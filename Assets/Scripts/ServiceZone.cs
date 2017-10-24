3d4b30a83147671bbb7075b111b25b4fc8efaaa1
Louis Groisne <louis.groisne@gmail.com>
2017-10-24 10:47:24 +0200

    Add the ServiceZone Script

---
+++ Assets/Scripts/ServiceZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceZone : MonoBehaviour
+{
+
+    private bool isValid = false;
+
+    public void SetIsValid(bool newValidityValue)
+    {
+        isValid = newValidityValue;
+    }
+    public bool GetIsValid()
+    {
+        return isValid;
+    }
+}
