a7ee538c8f3f3c57ec558a6f9ac7225db09af19f
Louis <louis.groisne@gmail.com>
2017-11-15 20:39:43 +0100

    Add a method that returns a random team in the enum

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -7,6 +7,7 @@ public class Utility {
 
     public enum Team { blue, red };
     static public int winningScore = 15;
+
     static public Team Opp(Team team)
     {
         Team opp;
@@ -21,5 +22,13 @@ public class Utility {
             throw new Exception("Error : Unrecognized Team passed to method Utility.Opp()");
         }
         return opp;
+    }
+
+    static public Team RandomTeam()
+    {
+        Array teams = Enum.GetValues(typeof(Team));
+        System.Random random = new System.Random();
+        Team randomTeam = (Team)teams.GetValue(random.Next(teams.Length));
+        return randomTeam;
     }
 }
