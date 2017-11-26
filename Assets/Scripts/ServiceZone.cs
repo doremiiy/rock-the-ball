39bf7949aedc7a27930adffa7d53ef8dd38b47b0
Louis Groisne <louis.groisne@gmail.com>
2017-11-26 14:30:43 +0100

    Add collision logic for the serviceZone Script

--- Assets/Scripts/ServiceZone.cs
+++ Assets/Scripts/ServiceZone.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ServiceZone : MonoBehaviour
 {
 
     private bool isValid = false;
+    private ServiceManager serviceManager;
 
     public bool IsValid
     {
@@ -17,6 +16,27 @@ public class ServiceZone : MonoBehaviour
         set
         {
             isValid = value;
+        }
+    }
+
+    public ServiceManager ServiceManager
+    {
+        get
+        {
+            return serviceManager;
+        }
+
+        set
+        {
+            serviceManager = value;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            serviceManager.HandleService(IsValid);
         }
     }
 }
