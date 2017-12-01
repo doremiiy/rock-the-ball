758a5fcde6db7945d66d8d01df887f83e6c6db56
Louis Groisne <louis.groisne@gmail.com>
2017-12-01 11:45:43 +0100

    Add sound support in the main menu

---
+++ Assets/Scripts/MenuSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSound : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public AudioClip welcomeClip;
+
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(WaitAndPlay(2f));
+	}
+
+    IEnumerator WaitAndPlay(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        audioSource.PlayOneShot(welcomeClip);
+    }
+}
