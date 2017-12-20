3ab28205077288acc1c29ab7af5ec3c30a3f32b4
Louis Groisne <louis.groisne@gmail.com>
2017-12-20 19:54:11 +0100

    Disable the racket's collider for 0.1s after a hit

--- Assets/Scripts/RacketController.cs
+++ Assets/Scripts/RacketController.cs
@@ -1,21 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
-public class RacketController : MonoBehaviour {
+public class RacketController : MonoBehaviour
+{
 
     public PlayerController playerController;
     public SoundManager soundManager;
     public Utility.Hand hand;
     public bool shouldVibrate;
+    private bool isActive;
+
+    public bool IsActive
+    {
+        get
+        {
+            return isActive;
+        }
+
+        set
+        {
+            isActive = value;
+            if (!IsActive)
+            {
+                StartCoroutine(ReactivateCollider(0.1f));
+            }
+        }
+    }
 
     void Start()
     {
         soundManager = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<SoundManager>();
     }
 
-    
-	void OnTriggerEnter (Collider collider) {
-        if (collider.CompareTag("Ball"))
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Ball") && IsActive)
         {
+            IsActive = false;
             Debug.Log("Racket Controller : collision detected");
             playerController.BallHit(hand);
             soundManager.PlaySound("RacketHit");
@@ -24,5 +46,11 @@ public class RacketController : MonoBehaviour {
                 //GetComponent<OVRVibration>().VibrateController(Utility.viveControllerNode, 5, 500);
             }
         }
-	}
+    }
+
+    IEnumerator ReactivateCollider(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        IsActive = true;
+    }
 }
